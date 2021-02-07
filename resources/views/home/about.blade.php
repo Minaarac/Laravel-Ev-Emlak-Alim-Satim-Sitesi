1c2677bb1ca3434e19f998c2c018743032429a83
Minaarac <minaarac@gmail.com>
2021-02-08 02:22:41 +0300

    Ana Template içerik düzenleme

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,4 +1,3 @@
-@extends('layouts.home')
 
 @section('title', 'About Us-' .  $setting->title)
 
@@ -6,40 +5,81 @@
 
 @section('keywords',$setting->keywords)
 
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <meta name="description" content="@yield('description')">
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="author" content="Mina Arac Property">
 
-@section('content')
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
 
-    <section id="aa-property-header">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="aa-property-header-inner">
-                        <h2>Blank Page</h2>
-                        <ol class="breadcrumb">
-                            <li><a href="{{route('homepage')}}">HOME</a></li>
-                            <li> <a href="#"> </a> About Us</li>
-                        </ol>
-                    </div>
+    <!-- Font awesome -->
+    <link href="{{ asset('assets')}}/css/font-awesome.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="{{ asset('assets')}}/css/bootstrap.css" rel="stylesheet">
+    <!-- slick slider -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets')}}/css/slick.css">
+    <!-- price picker slider -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets')}}/css/nouislider.css">
+    <!-- Fancybox slider -->
+    <link rel="stylesheet" href="{{ asset('assets')}}/css/jquery.fancybox.css" type="text/css" media="screen" />
+    <!-- Theme color -->
+    <link id="switcher" href="{{ asset('assets')}}/css/theme-color/default-theme.css" rel="stylesheet">
+
+    <!-- Main style sheet -->
+    <link href="{{ asset('assets')}}/css/style.css" rel="stylesheet">
+
+
+    <!-- Google Font -->
+    <link href='https://fonts.googleapis.com/css?family=Vollkorn' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+@include('home._header')
+
+<section id="aa-error-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="aa-error-area">
+                    <h3> <div align="center"><p>About Us </p> </div></h3>
+                    <ol class="breadcrumb">
+                        <li><a href="{{route('homepage')}}">HOME</a></li>
+                        <li> About Us</li>
+                    </ol>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <section id="aa-error">
-        <div class="container">
-            <row>
+<section id="aa-error">
+    <div class="container">
+        <row>
 
-                {!! $setting->aboutus !!}
+            {!! $setting->aboutus !!}
 
-                <div class="col-md-12">
-                    <div class="aa-error-area">
-                        <h4> </h4>
+            <div class="col-md-12">
+                <div class="aa-error-area">
+                    <h4> </h4>
 
-                    </div>
                 </div>
-            </row>
-        </div>
-    </section>
-
-
-@endsection
+            </div>
+        </row>
+    </div>
+</section>
+@include('home._footer')
--- resources/views/home/user_profile.blade.php
+++ resources/views/home/user_profile.blade.php
@@ -1,17 +1,61 @@
-@extends('layouts.home')
+
 
 @section('title','User Profile')
 
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <meta name="description" content="@yield('description')">
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="author" content="Mina Arac Property">
 
-@section('content')
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+
+    <!-- Font awesome -->
+    <link href="{{ asset('assets')}}/css/font-awesome.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="{{ asset('assets')}}/css/bootstrap.css" rel="stylesheet">
+    <!-- slick slider -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets')}}/css/slick.css">
+    <!-- price picker slider -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets')}}/css/nouislider.css">
+    <!-- Fancybox slider -->
+    <link rel="stylesheet" href="{{ asset('assets')}}/css/jquery.fancybox.css" type="text/css" media="screen" />
+    <!-- Theme color -->
+    <link id="switcher" href="{{ asset('assets')}}/css/theme-color/default-theme.css" rel="stylesheet">
+
+    <!-- Main style sheet -->
+    <link href="{{ asset('assets')}}/css/style.css" rel="stylesheet">
 
 
-    <section id="aa-property-header">
+    <!-- Google Font -->
+    <link href='https://fonts.googleapis.com/css?family=Vollkorn' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+@include('home._header')
+
+
+
+    <section id="aa-error-area">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="aa-property-header-inner">
-                        <h2>User Page</h2>
+                    <div class="aa-error-area">
+                        <h2> <div align="center"><p> User Page </p> </div></h2>
                         <ol class="breadcrumb">
                             <li><a href="{{route('homepage')}}">HOME</a></li>
                             <li> <a href="#"> </a> User Profile</li>
@@ -38,4 +82,4 @@
         </div>
     </section>
 
-@endsection
+
